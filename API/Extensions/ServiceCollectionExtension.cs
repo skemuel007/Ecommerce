4b8fdab908882630b861b5641a59e0d82a8fdac0
Stanley-Kemuel Lloyd Salvation <skemuel007@gmail.com>
2023-12-08 23:38:51 +0100

    End of module 2

--- API/Extensions/ServiceCollectionExtension.cs
+++ API/Extensions/ServiceCollectionExtension.cs
@@ -1,3 +1,6 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace API.Extensions;
 
 public static class ServiceCollectionExtension
@@ -9,5 +12,42 @@ public static class ServiceCollectionExtension
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
+    }
+
+    public static async Task ConfigureWebApplicationServices(this WebApplication app, IConfiguration configuration)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        using var scope = app.Services.CreateScope();
+        var services = scope.ServiceProvider;
+        var context = services.GetRequiredService<EcommerceContext>();
+// Create an instance of ILogger specifically for EcommerceContextSeed
+        var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+        var logger = loggerFactory.CreateLogger<EcommerceContextSeed>();
+
+        try
+        {
+            await context.Database.MigrateAsync();
+            // Create an instance of EcommerceContextSeed
+            var ecommerceContextSeed = new EcommerceContextSeed(logger);
+            await ecommerceContextSeed.SeedDataAsync(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occured during migration");
+        }
+
+        app.Run();
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -4,24 +4,10 @@ using Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddApiServices(builder.Configuration);
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
 
 var app = builder.Build();
+await app.ConfigureWebApplicationServices(builder.Configuration);
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
